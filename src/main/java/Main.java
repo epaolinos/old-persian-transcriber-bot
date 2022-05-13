d512a9757f2cd67b26e70c485e34be0335ae6ede
epaolinos <paulin2@yandex.ru>
2022-05-13 11:29:20 +0400

    Refactoring started

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,115 @@
-package PACKAGE_NAME;public class Main {
+import org.glassfish.jersey.jaxb.internal.XmlCollectionJaxbProvider;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class Main extends TelegramLongPollingBot {
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new Main());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try{
+            setButtons(sendMessage);
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if(message != null && message.hasText()){
+            String result = answer(message.getText());
+            sendMsg(message, result);
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "OldPersianTranscriberBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        String token = "";
+        Properties prop = new Properties();
+        try {
+            //load a properties file from class path, inside static method
+            prop.load(XmlCollectionJaxbProvider.App.class.getClassLoader().getResourceAsStream("config.properties"));
+
+            //get the property value and print it out
+            token = prop.getProperty("token");
+        } catch(IOException ex) {
+            ex.printStackTrace();
+        }
+        return token;
+    }
+
+    private void setButtons(SendMessage sendMessage) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        List<KeyboardRow> keyboardRowList = new ArrayList<>();
+        KeyboardRow keyboardRow1 = new KeyboardRow();
+
+        keyboardRow1.add(new KeyboardButton("/start"));
+        keyboardRow1.add(new KeyboardButton("/example"));
+        keyboardRow1.add(new KeyboardButton("/help"));
+
+        keyboardRowList.add(keyboardRow1);
+        replyKeyboardMarkup.setKeyboard(keyboardRowList);
+    }
+
+    private String answer(String input) {
+        switch (input) {
+            case("/start"): return
+                    "Hello. Let's transcribe your phrase into the Old Persian cuneiform." +
+                            "\nIf you just want to try it, click /example, copy the® phrase from the Behistun Inscription and send it back to the bot." +
+                            "\nTo learn some more info click /help." +
+                            "\nMade by @epaolinos (Pavel Egizaryan) in 2019.";
+            case("/help"): return
+                    "You can use digraphs instead of special characters:\n" +
+                            "\nā = a' " +
+                            "\nç = c' " +
+                            "\nθ = t' " +
+                            "\nš = s'" +
+                            "\n\nThe numbers for 200 and more are not known." +
+                            "\nMake sure your words and numbers are correctly separated with spaces." +
+                            "\nFor some words you will see possible logograms under the normal transcription.";
+            case("/example"): return
+                    "\n\nθātiy Dārayavauš xšāyaθiya vašnā Ahuramazdāha adam xšāyaθiya amiy Ahuramazdāha xšaçam manā frābara";
+        }
+
+        //return "Ok";
+
+        Transcriber transcriber = new Transcriber(input);
+        return transcriber.getTranscription();
+    }
 }
--- src/main/java/Transcriber.java
+++ src/main/java/Transcriber.java
@@ -1,2 +1,137 @@
-package PACKAGE_NAME;public class Transcriber {
+public class Transcriber{
+    private static String inputString;
+
+    public Transcriber(String input){
+        this.inputString = input;
+    }
+
+    public String getTranscription(){
+        return transcribe();
+    }
+    private String transcribe(){
+        String str = this.inputString.toLowerCase();
+        char[] inpArr = str.toCharArray();
+        StringBuilder builder = new StringBuilder();
+        builder.append("\uD800\uDFD0");
+
+        for(int i = 0; i < inpArr.length; i++) {
+            char ch = inpArr[i];
+            char next;
+            if (i < inpArr.length - 1) {
+                next = Character.toLowerCase(inpArr[i + 1]);
+            } else {
+                next = '\0';
+            }
+            if (ch == 'ā' || ch == 'a' && (i == 0 || next == '\'')) builder.append("\uD800\uDFA0");
+            if (ch == 'b') builder.append("\uD800\uDFB2");
+            if (ch == 'ç' || ch == 'c' && next == '\'') {builder.append("\uD800\uDFC2"); continue;}
+            if (ch == 'c') builder.append("\uD800\uDFA8");
+            if (ch == 'd') {
+                if (next == 'i') builder.append("\uD800\uDFAE");
+                else if (next == 'u') builder.append("\uD800\uDFAF");
+                else builder.append("\uD800\uDFAD");
+            }
+            if (ch == 'f') builder.append("\uD800\uDFB3");
+            if (ch == 'g') {
+                if (next == 'u') builder.append("\uD800\uDFA6");
+                else builder.append("\uD800\uDFA5");
+            }
+            if (ch == 'h') builder.append("\uD800\uDFC3");
+            if (ch == 'i') builder.append("\uD800\uDFA1");
+            if (ch == 'j') {
+                if (next == 'i') builder.append("\uD800\uDFA6");
+                else builder.append("\uD800\uDFA9");
+            }
+            if (ch == 'k') {
+                if (next == 'u') builder.append("\uD800\uDFA4");
+                else builder.append("\uD800\uDFA3");
+            }
+            if (ch == 'l') builder.append("\uD800\uDFBE");
+            if (ch == 'm') {
+                if (next == 'i') builder.append("\uD800\uDFB7");
+                else if (next == 'u') builder.append("\uD800\uDFB8");
+                else builder.append("\uD800\uDFB6");
+            }
+            if (ch == 'n') {
+                if (next == 'u') builder.append("\uD800\uDFB5");
+                else builder.append("\uD800\uDFB4");
+            }
+            if (ch == 'p') builder.append("\uD800\uDFB1");
+            if (ch == 'r') {
+                if (next == 'u') builder.append("\uD800\uDFBD");
+                else builder.append("\uD800\uDFBC");
+            }
+            if (ch == 'š' || ch == 's' && next == '\'') {builder.append("\uD800\uDFC1"); continue;}
+            if (ch == 's') builder.append("\uD800\uDFBF");
+            if (ch == 'θ' || ch == 't' && next == '\'') {builder.append("\uD800\uDFB0"); continue;}
+            if (ch == 't') {
+                if (next == 'u') builder.append("\uD800\uDFAC");
+                else builder.append("\uD800\uDFAB");
+            }
+            if (ch == 'u') builder.append("\uD800\uDFA2");
+            if (ch == 'v') {
+                if (next == 'i') builder.append("\uD800\uDFBB");
+                else builder.append("\uD800\uDFBA");
+            }
+            if (ch == 'x') builder.append("\uD800\uDFA7");
+            if (ch == 'y') builder.append("\uD800\uDFB9");
+            if (ch == 'z') builder.append("\uD800\uDFC0");
+            if (ch == ' ') {
+                if (next == '\n') {
+                    builder.append(" \n\uD800\uDFD0");
+                    i++;
+                } else {
+                    builder.append(" \uD800\uDFD0");
+                }
+            }
+            if (ch == '\n') builder.append("\n");
+            if (ch == '1') {
+                if (i == inpArr.length - 1 || next == ' ') builder.append("\uD800\uDFD1");
+                else if (i + 1 == inpArr.length - 1 || inpArr[i + 2] == ' ') builder.append("\uD800\uDFD3");
+                else builder.append("\uD800\uDFD5");
+            }
+            if (ch == '2') {
+                if (i == inpArr.length - 1 || next == ' ') builder.append("\uD800\uDFD2");
+                else builder.append("\uD800\uDFD4");
+            }
+            if (ch == '3') {
+                if (i == inpArr.length - 1 || next == ' ') builder.append("\uD800\uDFD2\uD800\uDFD1");
+                else builder.append("\uD800\uDFD4\uD800\uDFD3");
+            }
+            if (ch == '4') {
+                if (i == inpArr.length - 1 || next == ' ') builder.append("\uD800\uDFD2\uD800\uDFD2");
+                else builder.append("\uD800\uDFD4\uD800\uDFD4");
+            }
+            if (ch == '5') {
+                if (i == inpArr.length - 1 || next == ' ') builder.append("\uD800\uDFD2\uD800\uDFD2\uD800\uDFD1");
+                else builder.append("\uD800\uDFD4\uD800\uDFD4\uD800\uDFD3");
+            }
+            if (ch == '6') {
+                if (i == inpArr.length - 1 || next == ' ') builder.append("\uD800\uDFD2\uD800\uDFD2\uD800\uDFD2");
+                else builder.append("\uD800\uDFD4\uD800\uDFD4\uD800\uDFD4");
+            }
+            if (ch == '7') {
+                if (i == inpArr.length - 1 || next == ' ') builder.append("\uD800\uDFD2\uD800\uDFD2\uD800\uDFD2\uD800\uDFD1");
+                else builder.append("\uD800\uDFD4\uD800\uDFD4\uD800\uDFD4\uD800\uDFD3");
+            }
+            if (ch == '8') {
+                if (i == inpArr.length - 1 || next == ' ') builder.append("\uD800\uDFD2\uD800\uDFD2\uD800\uDFD2\uD800\uDFD2");
+                else builder.append("\uD800\uDFD4\uD800\uDFD4\uD800\uDFD4\uD800\uDFD4");
+            }
+            if (ch == '9') {
+                if (i == inpArr.length - 1 || next == ' ') builder.append("\uD800\uDFD2\uD800\uDFD2\uD800\uDFD2\uD800\uDFD2\uD800\uDFD1");
+                else builder.append("\uD800\uDFD4\uD800\uDFD4\uD800\uDFD4\uD800\uDFD4\uD800\uDFD3");
+            }
+        }
+
+        builder.append("\n");
+        if (str.contains("baga")) builder.append("\nLogogram for baga: \uD800\uDFCE");
+        if (str.contains("ahuramazda'") || str.contains("ahuramazdā")) builder.append("\nLogograms for Ahuramazdā: \uD800\uDFC8 / \uD800\uDFC9 / \uD800\uDFCA");
+        if (str.contains("dahya'u") || str.contains("dahyāu")) builder.append("\nLogograms for dahyāu: \uD800\uDFCC / \uD800\uDFCD");
+        if (str.contains("xs'a'yat'iya") || str.contains("xšāyaθiya")) builder.append("\nLogogram for xšāyaθiya: \uD800\uDFCB");
+        if (str.contains("θātiy dārayavauš xšāyaθiya vašnā ahuramazdāha adam xšāyaθiya amiy ahuramazdāha xšaçam manā frābara"))
+            builder.append("\n\nBy the way, it means \"King Darius says: By the grace of Ahuramazda am I king; Ahuramazda has granted me the kingdom.\"");
+
+        return builder.toString();
+    }
 }
